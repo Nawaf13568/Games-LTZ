47a11beb63e453fb943c94ad82458cc3ee73c1ac
FIKO665 <41846243+FIKO665@users.noreply.github.com>
2018-09-30 14:06:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,19 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
-
+const fs = require('fs'); 
+ 
 console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
 console.log('         [Wait please .. ]       ')
 console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
 client.on('ready', () => {
-	console.log('')
-	console.log('')
-	console.log('')
-	console.log('')
-	console.log('')
-	console.log('')
-	console.log('')
-	console.log('')
+    console.log('')
+    console.log('')
+    console.log('')
+    console.log('')
+    console.log('')
+    console.log('')
+    console.log('')
+    console.log('')
   console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
   console.log(`Logged in as [ ${client.user.tag}! ]`);
   console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
@@ -28,22 +28,22 @@ client.on('ready', () => {
   console.log(`channels! [ " ${client.channels.size} " ]`);
   console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
 });
-
-
-
-
+ 
+ 
+ 
+ 
 client.on('ready', () => {
   client.user.setGame(`!help | MALDYF`,'https://www.twitch.tv/v5bz');
 });
-
-
+ 
+ 
 let points = JSON.parse(fs.readFileSync('points.json', 'utf8'));
 client.on('message', message => {
     if (!points[message.author.id]) points[message.author.id] = {points : 0}
     if (message.content == 'نقاطي'){
-        var embed = new RichEmbed()
+        var embed = new Discord.RichEmbed()
         .setAuthor(message.author.username,message.author.avatarURL)
-        .addField(`نقاطك : ${points[message.author.id].points}`,'SYTRA',   true)
+        .addField(` نقاطك : ${points[message.author.id].points}`,'SYTRA',   true)
         .setColor('RANDOM')
         .setFooter('العاب وبس', client.user.avatarURL);
         message.channel.sendEmbed(embed)
@@ -52,7 +52,7 @@ client.on('message', message => {
         var x = ['ضفدع', 'طيارة', 'رعودي', 'تفكيك', 'تجربة', 'مدرسة', 'معلم' , 'نقاط' , 'اكسيفو' , 'مكوه' , 'هكونا مطاطا' , 'اكسيفو ذا بيست'];
         var x2 = ['ض ف د ع', 'ط ي ا ر ة', 'ر ع و د ي', 'ت ف ك ي ك', 'ت ج ر ب ة', 'م د ر س ة', 'م ع ل م', 'ن ق ا ط', 'ا ك س ي ف و', 'م ك و ه', 'ه ك و ن ا م ط ا ط ا', 'ا ك س ي ف و ذ ا ب ي س ت'];
         var x3 = Math.floor(Math.random()*x.length)
-        message.channel.send(`فكك الكلمة الآتية :${x[x3]}, لديك 20 ثانية`).then(msg1=> {
+        message.channel.send(`فكك الكلمة الآتية :${x[x3]} لديك 20 ثانية`).then(msg1=> {
             var r = message.channel.awaitMessages(msg => msg.content == x2[x3], {
                 maxMatches : 1,
                 time : 20000,
@@ -63,10 +63,10 @@ client.on('message', message => {
                     message.channel.sendEmbed(embed)
         })
         r.then(s=> {
-
-            points[message.author.id].points +=1
+ 
+        points[message.author.id].points +=1
             message.channel.send(`✅ لقد قمت بكتابة الجواب الصحيح بالوقت المناسب
- ─═════**{نقاطك:${points[message.author.id].points}}**═════─`);
+ ─═════**{ نقاطك :${points[message.author.id].points }}**═════─`);
                message.channel.sendEmbed(embed)
         })
         })
@@ -74,9 +74,9 @@ client.on('message', message => {
     fs.writeFile('points.json', JSON.stringify(points), (err) => {
         if (err) console.error(err);
     })
-	    if (message.content == "ركب") {    
+        if (message.content == "ركب") {    
         var x = ['ض ف د ع', 'ط ي ا ر ة', 'ر ع و د ي', 'ت ف ك ي ك', 'ت ج ر ب ة', 'م د ر س ة', 'م ع ل م', 'ن ق ا ط', 'ا ك س ي ف و', 'م ك و ه', 'ر و ق ن'];
-		var x2 = ['ضفدع', 'طيارة', 'رعودي', 'تفكيك', 'تجربة', 'مدرسة', 'معلم' , 'نقاط' , 'اكسيفو' , 'مكوه' , 'هكونا مطاطا' , 'روقن'];
+        var x2 = ['ضفدع', 'طيارة', 'رعودي', 'تفكيك', 'تجربة', 'مدرسة', 'معلم' , 'نقاط' , 'اكسيفو' , 'مكوه' , 'هكونا مطاطا' , 'روقن'];
         var x3 = Math.floor(Math.random()*x.length)
         message.channel.send(`ركب الكلمة  الآتية :${x[x3]}, لديك 20 ثانية`).then(msg1=> {
             var r = message.channel.awaitMessages(msg => msg.content == x2[x3], {
@@ -89,10 +89,10 @@ client.on('message', message => {
                     message.channel.sendEmbed(embed)
         })
         r.then(s=> {
-
+ 
             points[message.author.id].points +=1
             message.channel.send(`✅ لقد قمت بكتابة الجواب الصحيح بالوقت المناسب
- ─═════**{نقاطك:${points[message.author.id].points}}**═════─`);
+ ─═════**{ نقاطك :${points[message.author.id].points }}**═════─`);
                message.channel.sendEmbed(embed)
         })
         })
@@ -100,9 +100,9 @@ client.on('message', message => {
     fs.writeFile('points.json', JSON.stringify(points), (err) => {
         if (err) console.error(err);
     })
-	    if (message.content == "رياضيات") {    
+        if (message.content == "احسب") {    
         var x = ['50×50', '1000000×1', '89×10', '90×5', '30×3', '10×10', '1000×1000', '44,5+44,5'];
-		var x2 = ['2500', '1000000', '890', '450', '90', '100', '1000000' , '89'];
+        var x2 = ['2500', '1000000', '890', '450', '90', '100', '1000000' , '89'];
         var x3 = Math.floor(Math.random()*x.length)
         message.channel.send(`ركب الكلمة  الآتية :${x[x3]}, لديك 20 ثانية`).then(msg1=> {
             var r = message.channel.awaitMessages(msg => msg.content == x2[x3], {
@@ -115,10 +115,10 @@ client.on('message', message => {
                     message.channel.sendEmbed(embed)
         })
         r.then(s=> {
-
+ 
             points[message.author.id].points +=1
             message.channel.send(`✅ لقد قمت بكتابة الجواب الصحيح بالوقت المناسب
- ─═════**{نقاطك:${points[message.author.id].points}}**═════─`);
+ ─═════**{ نقاطك :${points[message.author.id].points}}**═════─`);
                message.channel.sendEmbed(embed)
         })
         })
@@ -126,26 +126,26 @@ client.on('message', message => {
     fs.writeFile('points.json', JSON.stringify(points), (err) => {
         if (err) console.error(err);
     })
-	
+   
   if (message.content == "عواصم") {
         var x = ['اليمن', 'مصر', 'الجزائر', 'السعودية', 'الصومال', 'العراق' , 'الامارات' , 'سوريا' , 'المغرب'];
         var x2 = ['صنعاء', 'القاهرة', 'الجزائر', 'الرياض', 'الخرطوم', 'بغداد', 'ابو ظبي','دمشق ','الر باط'];
         var x3 = Math.floor(Math.random()*x.length)
         message.channel.send(`ماهي عاصمة :${x[x3]}, لديك 15 ثانية`).then(msg1=> {
             var r = message.channel.awaitMessages(msg => msg.content == x2[x3], {
-                maxMatches : 1, 
-                time : 15000, 
-                errors : ['time'] 
+                maxMatches : 1,
+                time : 15000,
+                errors : ['time']
             })
         r.catch(() => {
-            return message.channel.send('❌ لقد انتهى الوقت ولم يقم أحد بالأجابة بشكل صحيح') 
+            return message.channel.send('❌ لقد انتهى الوقت ولم يقم أحد بالأجابة بشكل صحيح')
                message.channel.sendEmbed(embed)
         })
         r.then(s=> {
-
+ 
             points[message.author.id].points +=1
             message.channel.send(`✅ لقد قمت بكتابة الجواب الصحيح بالوقت المناسب
- ─═════**{نقاطك:${points[message.author.id].points}}**═════─`);
+ ─═════**{ نقاطك :${points[message.author.id].points}}**═════─`);
                message.channel.sendEmbed(embed)
         })
         })
@@ -164,14 +164,14 @@ client.on('message', message => {
                 errors : ['time']
             })
         r.catch(() => {
-            return message.channel.send('❌ لقد انتهى الوقت ولم يقم أحد بالأجابة بشكل صحيح') 
+            return message.channel.send('❌ لقد انتهى الوقت ولم يقم أحد بالأجابة بشكل صحيح')
                message.channel.sendEmbed(embed)
         })
         r.then(s=> {
-
+ 
             points[message.author.id].points +=1
             message.channel.send(`✅ لقد قمت بكتابة الجواب الصحيح بالوقت المناسب
- ─═════**{نقاطك:${points[message.author.id].points}}**═════─`);
+ ─═════**{ نقاطك :${points[message.author.id].points}}**═════─`);
                message.channel.sendEmbed(embed)
         })
         })
@@ -194,53 +194,52 @@ client.on('message', message => {
                     message.channel.sendEmbed(embed)
         })
         r.then(s=> {
-
+ 
             points[message.author.id].points +=1
             message.channel.send(`✅ لقد قمت بكتابة الجواب الصحيح بالوقت المناسب
- ─═════**{نقاطك:${points[message.author.id].points}}**═════─`);
+ ─═════**{ نقاطك :${points[message.author.id].points}}**═════─`);
                message.channel.sendEmbed(embed)
         })
         })
     }
     fs.writeFile('points.json', JSON.stringify(points), (err) => {
         if (err) console.error(err);
-
-});
-
-	
-	
-	
-	
-	
-	
-	
-	client.on("message", message => {
+    })
+    });
+ 
+   
+   
+   
+   
+   
+   
+   
+    client.on("message", message => {
  if (message.content === "!help") {
-  const embed = new Discord.RichEmbed() 
+  const embed = new Discord.RichEmbed()
       .setColor("RANDOM")
-      .setDescription(` ✧▬▬▬▬▬▬ BOT Games ▬▬▬▬▬▬✧
-
+      .setDescription(` ✧▬▬▬▬▬ BOT Games ▬▬▬▬▬✧
+ 
 قم بتفكيك الجمل  ➼ فكك
-
+ 
 قم بتركيب الجمل  ➼ ركب
-
+ 
 قم بحل المسائل الرياضية  ➼ احسب
-
+ 
 قم بأيجاد العواصم المطلوبة ➼ عواصم
-
+ 
 قم بحل الألغاز ➼ لغز
-
+ 
 قم بتجميع الحروف ➼ تحدي
-
-لعرض النقاط الخاصة بك ➼ نقاطي 
-
- ✧▬▬▬▬▬▬ BOT Games ▬▬▬▬▬▬✧`)
+ 
+لعرض النقاط الخاصة بك ➼ نقاطي
+ 
+ ✧▬▬▬▬▬ BOT Games ▬▬▬▬▬✧`)
  .setFooter("By : FiKO || ໑ڪــɹ̤ᓅ ")
    message.channel.sendEmbed(embed)
-    
+   
    }
    });
 
-
-
+    
 client.login(process.env.BOT_TOKEN)
